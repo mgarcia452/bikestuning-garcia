e918e4a6d29ed76d70be3698f180e79e87ef1a06
mgarcia452 <matiasgarcia452@gmail.com>
2022-08-11 16:27:36 -0300

    new repo

---
+++ Desktop/Coderhouse/React/bikestuning-garcia/src/App.js
@@ -0,0 +1,25 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="../mrSparkle.png" className="App-logo" alt="logo" />
+        <p>
+          starting a new trip
+        </p>
+        <a
+          className="App-link"
+          href="https://www.youtube.com/channel/UCeRs7EQu3GdE_08BfO46stg"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          something funnier
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
